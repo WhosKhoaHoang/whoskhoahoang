2fbd8f4debf9fc288f2577f49376e7b20e4c78fa
Khoa Hoang <whoskhoahoang@gmail.com>
2020-02-09 17:43:30 +0900

    Implement post edit page.
    TODO: Study how RoR handles routing.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,45 @@ class PostsController < ApplicationController
         @post = Post.new
     end
     
-    def create #Assoicated with a POST action?
+    
+    #NOTE: show, edit, update are already special RoR keywords?
+    #Post show page path: [domain]/post/[post_id]
+    def show
+        @post = Post.find(params[:id]) #I.e., GET params
+        #^This variable becomes visible in show.html.erb
+    end
+    
+    
+    #Post edit page path: [domain]/post/[post_id]/edit
+    def edit
+        @post = Post.find(params[:id])
+        #^This variable becomes visible in edit.html.erb
+    end
+    
+    
+    def create #Associated with a (HTTP) POST action?
         #render plain: params[:post].inspect
         @post = Post.new(post_params)
         if @post.save
             flash[:notice] = "Post was successfully created"
             redirect_to post_path(@post)
         else
-            render "new"
+            render "new" #I.e., new.html.erb
         end
     end
     
-    def show
+    
+    def update #Also associated with a (HTTP) POST action?
         @post = Post.find(params[:id])
+        if @post.update(post_params)
+            flash[:notice] = "Post was updated!"
+            redirect_to post_path(@post)
+        else
+            flash[:notice] = "Post was not updated"
+            render "edit" #I.e., edit.html.erb
+        end
     end
+    
     
     private
         def post_params
