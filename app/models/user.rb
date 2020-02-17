a54cb64ecca8ba54f4eaac6cb054797ce56bc71a
Khoa Hoang <whoskhoahoang@gmail.com>
2020-02-17 22:18:45 +0900

    Add password field to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
     validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
     before_save { self.email = email.downcase }
+    has_secure_password
 end
