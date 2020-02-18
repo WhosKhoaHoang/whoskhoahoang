fbe2c4905d86fb7428f83be80af3ba2301d71075
Khoa Hoang <whoskhoahoang@gmail.com>
2020-02-18 21:51:16 +0900

    Implement frontend for user registration.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
     root "pages#index"
     get "about", to: "pages#about"
     resources :posts #Gives all sorts of paths to update, delete, create, show
+    get "signup", to: "users#new"
+    resources :users, except: [:new]
 end
