659ca58774a7303112122c866c47654a0552648b
Khoa Hoang <whoskhoahoang@gmail.com>
2020-03-14 17:18:17 +0900

    Add styling to ArnoldBot chatbox.
    The chatbox has been centered and a scrollbar
    will appear if messages overflow the window
    containing the messages.

--- app/controllers/arnoldbot_controller.rb
+++ app/controllers/arnoldbot_controller.rb
@@ -3,7 +3,7 @@ class ArnoldbotController < ApplicationController
     def show        
     end
 
-
+    #NOTE: This action can only be called from an AJAX request!
     def msg_arnoldbot
         uri = URI("https://arnoldbot-api.herokuapp.com/talk?msg="+params[:msg])
         resp = Net::HTTP.get(uri)
